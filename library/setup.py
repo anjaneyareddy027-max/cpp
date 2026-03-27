831a4865d945aac67fcf5c81a99c6bb288e796a4
anjaneyareddy027-max <anjaneya.reddy027@gmail.com>
2026-03-27 16:47:53 +0530

    Rename PyPI package to studyhub-collab-nci

--- library/setup.py
+++ library/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="studyhub-nci",
+    name="studyhub-collab-nci",
     version="1.0.0",
     author="Anjaneya Reddy",
     description=(
